d160462cff6e386045a7c2ef537c303473e8241e
Javel Queiroz Freitas <javelqueiroz@outlook.com.br>
2021-09-21 17:47:04 -0300

    Header Component: done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Header } from './components/Header/Header';
 import { GlobalStyle } from './styles/global';
 
 const Title = styled.h1`
@@ -9,9 +10,7 @@ const Title = styled.h1`
 export function App() {
   return (
     <div className="App">
-        <h1>
-          Hello World
-        </h1>
+        <Header />
         <GlobalStyle/>
     </div>
   );
