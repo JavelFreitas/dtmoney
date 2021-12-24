06760edeb04e182a105558e408629aeb995257ce
Javel Queiroz Freitas <javelqueiroz@alu.ufc.br>
2021-12-24 15:28:11 -0300

    feat: implemented TransactionsProvider and realocated requisition responsability

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import Modal from 'react-modal'
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,13 +19,13 @@ export function App() {
         setIsNewTransactionModalOpen(false)
     }
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onHandleOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle/>
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
